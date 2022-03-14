4fc8e2f7325b8a0eb54d7ca69c4cd120641e3853
Luis Carlos <crluis521@gmail.com>
2022-03-13 21:28:38 -0300

    primeiro commit extensao

---
+++ myscript.js
@@ -0,0 +1,6 @@
+document.querySelector('.header-menu').style.fontStyle = 'italic';
+document.querySelector('.header-menu').style.backgroundColor = 'purple';
+document.querySelector('.header-menu').style.fontFamily = 'Ink Free';
+document.querySelector('.header-menu').style.borderRadius = "25px";
+document.querySelector('.header-menu').style.direction = "rtl";
+// document.querySelector('.header-menu').style.display ="none";
